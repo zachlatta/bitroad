72f2de2f93ef0b339afd392c558b7f9b3b3d42af
Zach Latta <zach@zachlatta.com>
2013-12-07 12:06:45 -0800

    Custom page title.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis
+  def full_title(page_title)
+    base_title = "Bitroad"
+    if page_title.empty?
+      base_title
+    else
+      "#{ page_title } | #{ base_title }"
+    end
+  end
 end
