d9befea4392c9cedb088331511bb9cbf1a1e63f6
Zach Latta <zach@zachlatta.com>
2013-12-07 17:32:30 -0800

    Send transactions to the creator.

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'bitcoin-ruby'
 gem 'compass-rails', git: "git://github.com/Labradorom/compass-rails"
 gem 'figaro'
 gem 'friendly_id'
+gem 'coinbase'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,7 +1,11 @@
+require "coinbase"
+
 class InvoicesController < ApplicationController
   def show
     @invoice = Invoice.friendly.find(params[:id])
     if !@invoice.completed
+      coinbase = Coinbase::Client.new(Figaro.env.coinbase_secret)
+      coinbase.send_money @invoice.listing.bitcoin_address, @invoice.listing.price * 0.9
       @invoice.update_attributes!(completed: true)
     elsif @invoice.session_id != request.session_options[:id]
       redirect_to root_path
