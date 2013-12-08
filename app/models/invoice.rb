c7f45fb724b268fe69510494a5905829147e5c20
Zach Latta <zach@zachlatta.com>
2013-12-07 16:45:00 -0800

    Obfuscated file URIs aren't happened.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,14 +1,9 @@
-require 'figaro'
-
 class Invoice < ActiveRecord::Base
   extend FriendlyId
   friendly_id :code, use: :slugged
 
   belongs_to :listing
-  has_attached_file :file, {
-    url: "/system/:hash.:extension",
-    hash_secret: Figaro.env.bitpay_key
-  }
+  has_attached_file :file
 
   validates :file, presence: true
   validates :session_id, presence: true
